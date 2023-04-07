1531c9282c3d3caf2d458a4a8f91009982bc2af9
Ahmet Cevdet Öztürk <124789789+acoBOYZ@users.noreply.github.com>
2023-04-07 07:46:07 +0300

    setting change

--- base/base/settings/common_settings.py
+++ base/base/settings/common_settings.py
@@ -117,8 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder', # Add this line
+]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
@@ -158,8 +162,4 @@ COMPRESS_JS_FILTERS = [
 ]
 COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder', # Add this line
-]
+

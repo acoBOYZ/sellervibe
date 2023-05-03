7c1c7d370b2df91ecedb9345023448be943ea477
Ahmet Cevdet Öztürk <124789789+acoBOYZ@users.noreply.github.com>
2023-05-03 14:15:57 +0300

    submit

--- base/base/settings/production_settings.py
+++ base/base/settings/production_settings.py
@@ -32,4 +32,12 @@ DATABASES = {
 }
 
 STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'base/assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+SECURE_HSTS_SECONDS = 31536000  # This sets HSTS to 1 year (recommended value)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True

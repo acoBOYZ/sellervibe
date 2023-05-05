d7949df7e34550e3ccc1584f995956469ffbda9a
Ahmet Cevdet Öztürk <124789789+acoBOYZ@users.noreply.github.com>
2023-05-05 17:12:35 +0300

    commit

--- base/tool_mail/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToolMailConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tool_mail'
--- base/tool_mail/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-class UserEmails(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    emails = models.JSONField()

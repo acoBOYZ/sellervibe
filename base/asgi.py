97f463da135d6d037f5a49fba19cd3ddc64f30bf
Ahmet Cevdet Öztürk <124789789+acoBOYZ@users.noreply.github.com>
2023-06-05 20:04:33 +0300

    commit

--- base/asgi.py
+++ base/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings.production_settings')
 django.setup()
 
 from autoleads import routing

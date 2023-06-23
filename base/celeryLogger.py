3de5c369e0a2ae33909c733f788fe3334c62f7ee
Ahmet Cevdet Öztürk <124789789+acoBOYZ@users.noreply.github.com>
2023-06-23 19:16:27 +0300

    test

--- base/celeryLogger.py
+++ base/celeryLogger.py
@@ -4,6 +4,10 @@ import os
 from pathlib import Path
 import logging
 
+@after_setup_logger.connect
+def setup_loggers(logger, *args, **kwargs):
+    LoggingTask.setup_loggers(logger, *args, **kwargs)
+
 class LoggingTask(Task):
     logger = None
 
@@ -12,7 +16,6 @@ class LoggingTask(Task):
         cls.logger = logging.getLogger(__name__)
 
     @classmethod
-    @after_setup_logger.connect
     def setup_loggers(cls, logger, *args, **kwargs):
         formatter = logging.Formatter('%(asctime)s - %(message)s')
 
@@ -28,25 +31,21 @@ class LoggingTask(Task):
         info = f'Task success: {cls.name} [{task_id}]'
         cls.logger.info(info)
 
-    
     @classmethod
     def on_failure(cls, exc, task_id, args, kwargs, einfo):
         error = f'Task failure: {cls.name} [{task_id}] - Error: {exc}'
         cls.logger.error(error)
-
     
     @classmethod
     def on_retry(cls, exc, task_id, args, kwargs, einfo):
         warning = f'Task retry: {cls.name} [{task_id}] - Error: {exc}'
         cls.logger.warning(warning)
-
     
     @classmethod
     def after_return(cls, status, retval, task_id, args, kwargs, einfo):
         debug = f'Task return: {cls.name} [{task_id}] - {status}'
         cls.logger.debug(debug)
 
-    
     @classmethod
     def on_start(cls, task_id, args, kwargs):
         debug = f'Task start: {cls.name} [{task_id}]'
